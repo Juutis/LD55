580c2ca374ad38d4bc4ededbfca2b6935d5e94d1
Juutis <>
2024-04-14 02:30:27 +0300

    asdf

--- ZarguufsGraveyardAdventure/Assets/Scripts/Enemy/RunTowardsTargetEnemy.cs
+++ ZarguufsGraveyardAdventure/Assets/Scripts/Enemy/RunTowardsTargetEnemy.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RunTowardsTargetEnemy : MonoBehaviour
 {
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class RunTowardsTargetEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        NavMeshPath path = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
     }
 }
