91f27e04a48e54089bcc0928431dbf40b763bb6b
Juutis <>
2024-04-13 22:52:42 +0300

    add enemy prefabs

---
+++ ZarguufsGraveyardAdventure/Assets/Scripts/Enemy/CharacterAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Walk();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    public void Idle() {
+        anim.Play("idle");
+    }
+
+    public void Walk() {
+        anim.Play("walk");
+    }
+}
