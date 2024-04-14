d79d7d2f4d56a4bcee43904e492416d6fd70a045
Matias Pitkänen <M2tias@Gmail.com>
2024-04-14 13:59:20 +0300

    Item drops

--- ZarguufsGraveyardAdventure/Assets/Scripts/Entities/EnemyHealth.cs
+++ ZarguufsGraveyardAdventure/Assets/Scripts/Entities/EnemyHealth.cs
@@ -1,12 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField]
     private int health = 5;
+
+    [SerializeField]
+    private List<ItemDropChance> itemDrops;
+
+    [SerializeField]
+    private float itemDropChance;
+
     public void GetHit(int damage)
     {
         UIManager.main.ShowMessage(transform.position, $"-{damage}", Color.white);
@@ -20,6 +25,30 @@ public class EnemyHealth : MonoBehaviour
     public void Die()
     {
         Debug.Log("Enemy died!");
+
+        if (Random.Range(0,1f) <= itemDropChance)
+        {
+            List<InventoryItemConfig> itemChoices = new();
+            foreach (ItemDropChance item in itemDrops)
+            {
+                for (int i = 0; i < item.DropChanceMultiplier; i++)
+                {
+                    itemChoices.Add(item.Item);
+                }
+            }
+
+            InventoryItemConfig drop = itemChoices[Random.Range(0, itemChoices.Count)];
+            PickupableItemManager.main.CreateItem(drop, transform.position);
+        }
+
         Destroy(gameObject);
     }
+}
+
+[System.Serializable]
+public class ItemDropChance
+{
+    public InventoryItemConfig Item;
+    // if any item drops, it's DropChanceMultiplier times more likely to be this item
+    public int DropChanceMultiplier;
 }
